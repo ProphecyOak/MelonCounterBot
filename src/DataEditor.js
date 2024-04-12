2655ee91883ea2386e7587a615205d49da435127
ProphecyOak <drito610@gmail.com>
2024-04-12 12:05:40 -0400

    Revert "Forum rework (#8)" (#9)
    This reverts commit 1f82d029547119cb714b5b34d49f31e977f58517.

---
+++ src/DataEditor.js
@@ -0,0 +1,82 @@
+const { dataAddress } = require("../config.json");
+const  db = require("./mongo.js");
+
+
+
+module.exports = {checkMessageHasImg, addReactionMelons, removePost, getYoungTime,
+wipeMainData, wipeYoungData, addPost, addPosts, getMelonAdders
+}
+
+//checkMessageHasImg(message): boolean
+async function checkMessageHasImg(message, fetchIt = true) {
+  let meloners = message.reactions.resolve("🍉");
+  if (fetchIt) {message = await message.fetch(false);}
+  return meloners !== null || message.attachments.filter((key, val) => {return key.contentType === 'image/png';}).size !== 0;
+}
+
+//Takes in a reaction and adds or subtracts that melon from the data.
+//addReactionMelons(reaction: MessageReaction, user: String, amount: number)
+async function addReactionMelons(reaction, user, amount) {
+  let author = reaction.message.author.id
+  let young = reaction.message.createdTimestamp > await getYoungTime();
+  db.incrementData("MainData", {"_id": user}, {"awarded": amount});
+  db.incrementData("MainData", {"_id": author}, {"received": amount});
+  if(young){
+    db.incrementData("YoungData", {"_id": user}, {"awarded": amount});
+    db.incrementData("YoungData", {"_id": author}, {"received": amount});
+  }
+}
+
+//decrements post count.
+//removePost(message: Message)
+async function removePost(message) {
+  let young = message.createdTimestamp > await getYoungTime();
+  let melonAdders = await getMelonAdders(message, false);
+  if (melonAdders === [] || melonAdders === undefined || melonAdders === null) return;
+  let melons = 0;
+  for (const m of melonAdders){
+    db.incrementData("MainData", {"_id": m}, {"awarded": -1});
+    if(young) db.incrementData("YoungData", {"_id": m}, {"awarded": -1});
+    melons++;
+  }
+  //Doesn't modify first post date. Unlikely removed is first post. Not fatal if it is.
+  db.removePostData("MainData", message.author.id, melons);
+  if(young) db.removePostData("YoungData", message.author.id, melons);
+}
+
+async function getYoungTime() {return db.getYoungTime();}
+
+function wipeYoungData() {return db.drop("YoungData")}
+function wipeMainData() {return db.drop("MainData")}
+
+//adds a new Post
+async function addPost (post){
+  db.addPostData("YoungData", post.author, 0, post.createdTimestamp);
+  db.addPostData("MainData", post.author, 0, post.createdTimestamp);
+}
+
+//add full recount posts to MainData
+async function addPosts (posts){
+  for (const p of posts){
+      let melonAdders = await getMelonAdders(p, true);
+      if (melonAdders === [] || melonAdders === undefined || melonAdders === null) continue;
+      let melons = 0
+      for (const m of melonAdders){
+        db.incrementData("MainData", {"_id": m}, {"awarded": 1});
+        melons++;
+      }
+      db.addPostData("MainData", p.author, melons, p.createdTimestamp);
+  }
+}
+
+async function getMelonAdders(message, fetchIt=true) {
+  let meloners = message.reactions.resolve("🍉");
+  if (meloners === null) {return []}
+  let melonAdders;
+  if (fetchIt) {
+    melonAdders = (await meloners.users.fetch()).keys();
+  } else {
+    melonAdders = meloners.users.cache.keys();
+  }
+  return melonAdders;
+}
--- src/index.js
+++ src/index.js
@@ -1,25 +1,20 @@
-//	--------    IMPORTS AND DEFAULTS    --------
+//IMPORTS AND DEFAULTS
 const fs = require('fs');
 const path = require('path');
 const { Client, Collection, Events, GatewayIntentBits, Partials } = require('discord.js');
 const { token, galleryChannelID, publicKey } = require('../config.json');
-
-const eventHandler = require("./eventHandler.js");
-const mongoInterface = require("./mongoInterface.js");
-const logHandler = require("./logHandler.js");
-const oldCounter = require("./oldCounter.js");
+const dataEditTools = require('./DataEditor.js');
+const counterTools = require('./countHelper.js');
 
 let startTime;
 
-//	--------    CREATE DISCORD CLIENT    --------
-
+//CREATE DISCORD CLIENT
 const client = new Client({
 	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions],
-	partials: [Partials.Message, Partials.Channel, Partials.Reaction, Partials.User],
+	partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 });
 
-//	--------    FETCH CLIENT COMMANDS    --------
-
+//FETCHING CLIENT COMMANDS
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
@@ -35,41 +30,33 @@ for (const file of commandFiles) {
 	}
 }
 
-//	--------    EVENT HANDLING    --------
-
-//	Forum post created
-client.on(Events.ThreadCreate, async thread => {
-	// console.log("Event: Thread Added");
-	if (thread.parentId === galleryChannelID) {
-		await eventHandler.postCreation(thread, client);
+//EVENT HANDLING
+client.once(Events.ClientReady, async () => {
+	startTime = Date.now();
+	console.log("Booting Up...")
+	console.log(await dataEditTools.getYoungTime());
+	if (await dataEditTools.getYoungTime() !== undefined) {await counterTools.checkYoungData(client);}
+	else {await counterTools.countAllMelons(client);}
+	console.log(`${(Date.now()-startTime)/1000} seconds elapsed.`);
+	console.log('Ready!');
+});
+client.on(Events.MessageReactionAdd, (reaction, user) => {
+  counterTools.reactChange(reaction, 1, user, galleryChannelID);
+});
+client.on(Events.MessageReactionRemove, (reaction, user) => {
+  counterTools.reactChange(reaction, -1, user, galleryChannelID);
+});
+client.on(Events.MessageCreate, async message => {
+	if (message.channelId !== galleryChannelID) {return;}
+	counterTools.checkIfRebuild(client); //Check on message if its time to build new YoungData
+	message = await message.fetch();
+	if (await dataEditTools.checkMessageHasImg(message)) {await dataEditTools.addPost(message);}
+});
+client.on(Events.MessageDelete, async message => {
+	if (await dataEditTools.checkMessageHasImg(message, false)) {
+		await dataEditTools.removePost(message);
 	}
 });
-
-//	Forum post deleted
-client.on(Events.ThreadDelete, async thread => {
-	// console.log("Event: Thread Removed");
-	if (thread.parentId === galleryChannelID) {
-		await eventHandler.postDeletion(thread, client);
-	}
-});
-
-//	Melon added to post
-client.on(Events.MessageReactionAdd, async (reaction, user) => {
-	// console.log("Event: Reaction Added");
-	if (reaction.emoji.name === '🍉') {
-		await eventHandler.melonAdded(reaction, user, client);
-	}
-});
-
-//	Melon removed from post
-client.on(Events.MessageReactionRemove, async (reaction, user) => {
-	// console.log("Event: Reaction Removed");
-	if (reaction.emoji.name === '🍉') {
-		await eventHandler.melonRemoved(reaction, user, client);
-	}
-});
-
-//	Slash command used
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 	const command = interaction.client.commands.get(interaction.commandName);
@@ -84,13 +71,6 @@ client.on(Events.InteractionCreate, async interaction => {
 	}
 });
 
-//	--------    LOGIN THE CLIENT    --------
 
-client.once(Events.ClientReady, async () => {
-	await mongoInterface.login();
-	await oldCounter.countAllFresh(client);
-	logHandler.logEvent("Ready!", logHandler.levels.STATUS);
-});
-
-logHandler.logEvent("Booting Up...", logHandler.levels.STATUS);
+//LOGIN THE CLIENT
 client.login(token);
